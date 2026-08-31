20f28ab4662b943d0b3d05a351ed13a6225b72a3
DarthM <mgracer48@yahoo.com>
2026-08-31 02:37:44 -0400

    config: set default Discord client ID

--- runtime/include/runtime_config.h
+++ runtime/include/runtime_config.h
@@ -832,7 +832,7 @@ inline bool DiscordPresenceEnabled(bool fallback = true) {
     return Get().discordPresenceEnabled.value_or(fallback);
 }
 
-inline std::string DiscordClientId(std::string fallback = "") {
+inline std::string DiscordClientId(std::string fallback = "1543869015015952485") {
     return Get().discordClientId.value_or(std::move(fallback));
 }
 
